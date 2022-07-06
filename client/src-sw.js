f4c76908c82e4583680378bbf3d911412cc0a0f0
Elias Vasquez <eliasvasquez7@gmail.com>
2022-07-06 14:02:32 -0600

    Added the imported the right workbox

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,16 +29,14 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Implement asset caching
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
+	({ request }) =>
+		['style', 'script', 'worker'].includes(request.destination),
+	new StaleWhileRevalidate({
+		cacheName: 'asset-cache',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200]
+			})
+		]
+	})
 );
